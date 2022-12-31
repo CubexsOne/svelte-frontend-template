cb48b51da40115bdd3b784d69f50ce909ab004f2
Pascal Sauer <email@cubexs.dev>
2022-12-31 19:28:38 +0100

    Add base style and font

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import App from './App.svelte'
 
+import 'normalize.css'
+import './assets/fonts.scss'
+import './assets/styles.scss'
+
 const app = new App({
   target: document.getElementById('app'),
 })
